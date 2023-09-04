844fc43b7ae442063602976fa17b8cb3c5c34a50
Md-Golam-Mostafa <gmshakilcse@gmail.com>
2023-09-04 10:10:00 +0600

    normal reverse

---
+++ NormalReverse.js
@@ -0,0 +1,5 @@
+const originalArray = [1, 2, 3, 4, 5];
+
+const reverseArray = originalArray.reverse();
+console.log(reverseArray);  
+//  [ 5, 4, 3, 2, 1 ]  ===>>  The output is after using reverse()
